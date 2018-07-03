16eea41a296ee1814b10d2c8391a5c9ee45d7617
delioda <delioda@consenteye.com>
2018-07-03 15:54:04 +0100

    COmpleted teh publisher

---
+++ publisher/server.go
@@ -0,0 +1,56 @@
+package publisher
+
+import (
+	"bitbucket.org/ConsentSystems/mango-micro/mango-service/registry/consul"
+	"bitbucket.org/ConsentSystems/mango-micro/mango-service/service"
+	"github.com/hashicorp/consul/api"
+	mangos "nanomsg.org/go-mangos"
+)
+
+// Server is the server
+type Server interface {
+	AddTransport(tr mangos.Transport)
+	Run(port int, transport, addr string)
+}
+type defaultServer struct {
+	server  service.Server
+	name    string
+	version string
+}
+
+// AddTransport adds a transport to the subscriber's socket
+func (pubs *defaultServer) AddTransport(tr mangos.Transport) {
+	pubs.server.Sock().AddTransport(tr)
+}
+
+// Run runs the subscriber
+func (pubs *defaultServer) Run(port int, transport, addr string) {
+	pubs.server.Run(
+		pubs.name,
+		addr,
+		port,
+		transport,
+		pubs.version,
+	)
+}
+
+func NewServer(
+	regAddr string,
+	serviceName string,
+	version string,
+) (Server, error) {
+	pubSock, err := NewPublisher()
+	if err != nil {
+		return nil, err
+	}
+	registry := consul.NewRegistry(&api.Config{
+		Address: regAddr,
+		Scheme:  "http",
+	})
+
+	server := service.NewMangoServer(pubSock, registry)
+
+	return &defaultServer{
+		server: server,
+	}, nil
+}
---
+++ publisher/socket.go
@@ -0,0 +1,43 @@
+package publisher
+
+import (
+	"encoding/json"
+
+	"nanomsg.org/go-mangos/protocol/pub"
+
+	"bitbucket.org/ConsentSystems/mango-micro/messages"
+	mangos "nanomsg.org/go-mangos"
+)
+
+type Publisher interface {
+	mangos.Socket
+	Publish(name string, message []byte) error
+}
+
+type defaultPubisher struct {
+	mangos.Socket
+}
+
+func (pubs *defaultPubisher) Publish(name string, message []byte) error {
+	trg := &messages.Trigger{
+		Name:   name,
+		Params: message,
+	}
+
+	bts, err := json.Marshal(trg)
+	if err != nil {
+		return err
+	}
+	pubs.Send(bts)
+	return nil
+}
+
+func NewPublisher() (Publisher, error) {
+	sock, err := pub.NewSocket()
+	if err != nil {
+		return nil, err
+	}
+	return &defaultPubisher{
+		Socket: sock,
+	}, nil
+}
